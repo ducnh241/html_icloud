a7dfd11611d880c2615b6f374969120224e84195
ducnhsmcorp <huuductin1k12@gmail.com>
2019-03-08 09:44:55 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("#sign-in").click(function(){
+  	// $('#sign_in_form').removeClass('hide-password');
+    $('#sign_in_form').addClass('show-password');
+  });
+});
